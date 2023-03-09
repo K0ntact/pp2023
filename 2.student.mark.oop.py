055c12ca6e7e7ddf3b35c9f4e9caa0d30294b7c8
Nguyen Duy An <annd.bi12-006@st.usth.edu.vn>
2023-03-09 19:10:44 +0700

    Rewrite Lab 2 for better compatibility with Lab 3

--- 2.student.mark.oop.py
+++ 2.student.mark.oop.py
@@ -1,200 +1,211 @@
-# courses_list will contain all instances of class Course:
-#   courses_list = [ads, oop, nm]
+# Class Info is the parent of class Student and class Course, has 2 basic attributes: ID and name
+# Class Student is inherited from Info and has 1 more attribute: DOB
+# Class Course is inherited from Info
+# Class MarkSheet will contain instances of class Course and class Student.
 #
-# Class Course will contain everything from the info of the course
-# to the students info and their marks.
+# Each Student instance will be stored in __student_list:
+#     __student_list = [std1, std2, std3]
 #
-# Each Student instance will contain the student info and stored in __std_list:
-#     __std_list = [std1, std2, std3]
-#
-# Student score will be stored in __marks as a dictionary with 2 keys: "name" and "mark"
-#     __marks = [
-#         {
-#             "name": "Student 1",
-#             "mark": 15
-#         },
-#         {
-#             "name": "Student 2",
-#             "mark": 13
-#         },
-#         {
-#             "name": "Student 3",
-#             "mark": 18
-#         }
-#     ]
+# Each Course instance will be stored in __class_list:
+#     __course_list = [course 1, course 2, course 3]
+
 import re
 
 
-class Student:
-    def __init__(self, Id: str, name: str, dob: str):
-        self.__id = Id
-        self.__name = name
+class COLORS:
+    HEADER = '\033[95m'
+    GREEN = '\033[92m'
+    RED = '\033[91m'
+    BLUE = '\033[94m'
+    ENDC = '\033[0m'
+    YELLOW = '\033[93m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+class Info:
+    def __init__(self, ID: str, name: str) -> None:
+        self._id = ID
+        self._name = name
+
+    def get_name(self) -> str: return self._name
+    def get_ID(self) -> str: return self._id
+
+    def display_info(self) -> None:
+        print(f"ID: {self._id}")
+        print(f"Name: {self._name}")
+
+
+class Student(Info):
+    def __init__(self, ID: str, name: str, dob: str) -> None:
+        super().__init__(ID, name)
         self.__dob = dob
 
-    def getID(self) -> str:
-        return self.__id
+    def get_dob(self) -> str: return self.__dob
 
-    def getName(self) -> str:
-        return self.__name
-
-    def getDob(self) -> str:
-        return self.__dob
-
-    def setID(self, iD: str) -> None:
-        self.__id = iD
-
-    def setName(self, name: str) -> None:
-        self.__name = name
-
-    def setDob(self, dob: str) -> None:
-        self.__dob = dob
+    def display_info(self) -> None:
+        print(f"ID: {self._id}")
+        print(f"Name: {self._name}")
+        print(f"DOB: {self.__dob}")
+        print("\n")
 
 
-class Course:
-    def __init__(self, Id: str, name: str):
-        self.__id = Id
-        self.__name = name
+class Course(Info):
+    def __init__(self, ID: str, name: str) -> None:
+        super().__init__(ID, name)
         self.__marks = []
-        self.__std_list = []
 
-    def getID(self) -> str:
-        return self.__id
+    def get_marks(self) -> list: return self.__marks
 
-    def getName(self) -> str:
-        return self.__name
+    def display_info(self) -> None:
+        print(f"ID: {self._id}")
+        print(f"Name: {self._name}")
+        print("\n")
 
-    def setID(self, iD: str) -> None:
-        self.__id = iD
+    def display_marks(self, std_list: list) -> None:
+        for std in std_list:
+            std_pos = std_list.index(std)
+            print(f"{std.get_name()}: {self.__marks[std_pos]}")
 
-    def setName(self, name: str) -> None:
-        self.__name = name
+    def set_marks(self, std_list: list) -> None:
+        for std in std_list:
+            mark = int(input(f"Set the score for student {std.get_name()}: "))
+            self.__marks.append(mark)
 
-    def addStudent(self) -> None:
+
+class MarkSheet:
+    def __init__(self):
+        self.__student_list = []
+        self.__course_list = []
+
+    def get_course_list(self) -> list: return self.__course_list
+    def get_student_list(self) -> list: return self.__student_list
+
+    def add_course(self) -> None:
+        name = input("Enter course name: ")
+        ID = input("Enter course ID: ")
+        new_course = Course(ID, name)
+        self.__course_list.append(new_course)
+
+    def add_student(self) -> None:
         name = input("Enter student's name: ")
 
-        Id = input("The ID should be BI/BAxx-xxx (x is from 0-9)\nEnter student id: ")
+        ID = input("The ID should be BI/BAxx-xxx (x is from 0-9)\nEnter student ID: ")
         while True:
-            checker = re.search('(BI|BA)[0-9][0-9]-[0-9][0-9][0-9]', Id)
+            checker = re.search('(BI|BA)[0-9][0-9]-[0-9][0-9][0-9]', ID)
             if checker is None:
-                print("You've typed in the wrong ID format.\nThe ID should be BI/BAxx-xxx (x is from 0-9)\n")
-                Id = input("Enter student's id: ")
+                print(f"{COLORS.RED}You've typed in the wrong ID format.{COLORS.ENDC}\nThe ID should be BI/BAxx-xxx (x is from 0-9)\n")
+                ID = input("Enter student's ID: ")
             else:
                 break
 
         dob = input("The DOB should be dd/mm/yyyy\nEnter student DOB: ")
         while True:
-            checker = re.search("[0-3][0-9]/[0-1][0-2]/[1-2][0-9[0-9][0-9]", dob)
+            checker = re.search('[0-3][0-9]/[0-1][0-9]/[1-2][0-9[0-9][0-9]', dob)
             if checker is None:
-                print("You've typed in the wrong DOB format.\nThe DOB should be dd/mm/yyyy\n")
-                Id = input("Enter student's id: ")
+                print(f"{COLORS.RED}You've typed in the wrong DOB format.{COLORS.ENDC}\nThe DOB should be dd/mm/yyyy\n")
+                ID = input("Enter student's DOB: ")
             else:
                 break
 
-        std = Student(Id, name, dob)
-        self.__std_list.append(std)
+        new_student = Student(ID, name, dob)
+        self.__student_list.append(new_student)
 
-    def setScore(self) -> None:
-        temp = {}
-        for std in self.__std_list:
-            mark = int(input(f"Enter mark for student {std.getName()}: "))
-            temp["name"] = std.getName()
-            temp["mark"] = mark
-            self.__marks.append(temp)
+    def list_course(self) -> None:
+        for course in self.__course_list:
+            course.display_info()
+        print("\n")
 
-    def showScore(self) -> None:
-        mark_list = self.__marks
-        for std in mark_list:
-            name = std["name"]
-            score = std["mark"]
-            print(f"{name} : {score}")
+    def list_student(self) -> None:
+        for student in self.__student_list:
+            student.display_info()
+        print("\n")
 
 
 def main():
-    courses_list = []
+    ms = MarkSheet()
     while True:
-        print("\n1. Add course\n2. Add student\n3. Set score\n 4.Show score\n---------\n0. Exit\n")
-        choice = int(input("Enter your choice: "))
-        # Check if the choice is correct
-        while True:
-            try:
-                if choice in range(0, 5):
-                    break
-            except:
-                print("Invalid choice!")
-            print("\n\n1. Enter course\n2. Enter student\n3. Set score\n 4. Show score\n---------\n0. Exit\n")
-            choice = int(input("Enter your choice: "))
-
+        print("\nStudent Mark Management System")
+        print("-"*20)
+        print(f"[{COLORS.YELLOW}1{COLORS.ENDC}] Add a student")
+        print(f"[{COLORS.YELLOW}2{COLORS.ENDC}] Add a course")
+        print(f"[{COLORS.YELLOW}3{COLORS.ENDC}] Input marks for a course")
+        print(f"[{COLORS.YELLOW}4{COLORS.ENDC}] List all student")
+        print(f"[{COLORS.YELLOW}5{COLORS.ENDC}] List all course")
+        print(f"[{COLORS.YELLOW}6{COLORS.ENDC}] Show student marks in a course")
+        print("-"*20)
+        print(f"[{COLORS.YELLOW}0{COLORS.ENDC}] Exit")
+        choice = int(input("Your choice: "))
         match choice:
-            # Exit the program
+            # Exit
             case 0:
-                print("Program terminated!")
+                print("Program terminated")
                 break
 
-            # Enter course info
+            # Add a student
             case 1:
-                name = input("Enter course's name: ")
-                Id = input("Enter course's id: ")
-                crs = Course(Id, name)
-                courses_list.append(crs)
+                ms.add_student()
 
-            # Enter student info for a course
+            # Add a course
             case 2:
-                if len(courses_list) == 0:
-                    print("No courses found! Please enter a course!\n")
+                ms.add_course()
 
-                else:
-                    print("Available courses: ")
-                    for c in courses_list:
-                        print(c.getName() + " ")
-                    name = input("Choose the course: ")
-
-                    found = False
-                    while not found:
-                        for c in courses_list:
-                            if name == c.getName():
-                                found = True
-                                c.addStudent()
-
-                        if not found:
-                            print("\nCourse name not found! Check that you enter the right name!")
-                            name = input("Choose the course: ")
-
-            # Set the marks for the students
+            # Input marks for a course
             case 3:
-                print("Available courses: ")
-                for c in courses_list:
-                    print(c.getName() + " ")
-                name = input("Choose the course: ")
+                if len(ms.get_course_list()) == 0:
+                    print(f"{COLORS.RED}You have to add a course first!{COLORS.ENDC}")
+                    continue
 
-                found = False
-                while not found:
-                    for c in courses_list:
-                        if name == c.getName():
-                            found = True
-                            c.setScore()
+                if len(ms.get_student_list()) == 0:
+                    print(f"{COLORS.RED}You have to add a student first!{COLORS.ENDC}")
+                    continue
 
-                    if not found:
-                        print("\nCourse name not found! Check that you enter the right name!")
-                        name = input("Choose the course: ")
+                print("Available course(s):", end=" ")
+                for course in ms.get_course_list():
+                    print(course.get_name(), end="  ")
+                name = input("\nYour choice: ")
 
-            # Show scores of a course
+                for course in ms.get_course_list():
+                    if name != course.get_name():
+                        print(f"{COLORS.RED}Course not found!{COLORS.ENDC}")
+                        break
+
+                    # course found
+                    course.set_marks(ms.get_student_list())
+
+            # List all students
             case 4:
-                name = input("Choose the course: ")
-                print("Available courses: ")
-                for c in courses_list:
-                    print(c.getName() + " ")
+                ms.list_student()
 
-                found = False
-                while not found:
-                    for c in courses_list:
-                        if name == c.getName():
-                            found = True
-                            c.showScore()
-                            break
+            # List all courses
+            case 5:
+                ms.list_course()
 
-                    if not found:
-                        print("\nCourse name not found! Check that you enter the right name!")
-                        name = input("Choose the course: ")
+            # Show all student marks in a course
+            case 6:
+                if len(ms.get_course_list()) == 0:
+                    print(f"{COLORS.RED}You have to add a course first!{COLORS.ENDC}")
+                    continue
+
+                if len(ms.get_student_list()) == 0:
+                    print(f"{COLORS.RED}You have to add a student first!{COLORS.ENDC}")
+                    continue
+
+                print("Available course(s):", end=" ")
+                for course in ms.get_course_list():
+                    print(course.get_name(), end="  ")
+                name = input("\nYour choice: ")
+
+                for course in ms.get_course_list():
+                    if name != course.get_name():
+                        print(f"{COLORS.RED}Course not found!{COLORS.ENDC}")
+                        break
+
+                    if course.get_marks():
+                        course.display_marks(ms.get_student_list())
+                    else:   # no marks in course
+                        print(f"{COLORS.RED}You haven't put any marks in this course!{COLORS.ENDC}")
+                        break
 
 
 if __name__ == "__main__":
