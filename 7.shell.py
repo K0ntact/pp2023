0cc1b2ffbe49114aa9e6941cc0a28a37ea700f0b
Kontact <duyan5745@gmail.com>
2023-03-23 22:15:12 +0700

    Add main()

--- 7.shell.py
+++ 7.shell.py
@@ -1,11 +1,17 @@
 import subprocess
 
-while True:
-    cmd = input('shell> ')
-    if cmd == 'exit':
-        break
-    try:
-        output = subprocess.check_output(cmd, shell=True)
-        print(output.decode('utf-8'))
-    except Exception as e:
-        print(e)
+
+def main():
+    while True:
+        cmd = input('shell> ')
+        if cmd == 'exit':
+            break
+        try:
+            output = subprocess.check_output(cmd, shell=True)
+            print(output.decode('utf-8'))
+        except Exception as e:
+            print(e)
+
+
+if __name__ == '__main__':
+    main()
